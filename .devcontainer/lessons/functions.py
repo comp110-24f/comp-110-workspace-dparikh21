7bcff88448795d3ca9f7f92d38693172169bc901
Diya Parikh <diyaparikh@diyas-air.wireless-1x.unc.edu>
2024-08-26 15:47:22 -0400

    add functions file

---
+++ .devcontainer/lessons/functions.py
@@ -0,0 +1,10 @@
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:
+    """Return the sum of num1 and num2."""
+    return num1 + num2
+
+#function call
+print(sum(num1 = 2, num2 = 13))
+
